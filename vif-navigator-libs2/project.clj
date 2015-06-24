ac86a758187f302aa1220d8e73cc1a1c60b3fb0a
mzheludkov <mzheludkov@at-consulting.ru>
2015-06-24 18:47:02 +0300

    typed clojure

--- vif-navigator-libs2/project.clj
+++ vif-navigator-libs2/project.clj
@@ -4,21 +4,30 @@
             :url "http://github"
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [net.sf.kxml/kxml2 "2.3.0" :use-resources true]
-                           [org.clojure/core.typed "0.3.0-alpha5"]
-                           ;[org.clojure/core.typed "0.2.50"]
-                           [reply/reply "0.1.0-beta9"]
-                           [org.clojure/tools.nrepl "0.2.6"]
+
+                           ;[reply/reply "0.1.0-beta9"]
+                           ;[org.clojure/tools.nrepl "0.2.6"]
                            ]
-            :profiles {:default  [:dev]
-                       :dev      {:dependencies   []
+            :profiles {:default  [:repl]
+                       :dev      {:dependencies   [
+                                                   [org.clojure/core.typed.rt "0.3.0-alpha5"]
+                                                   ]
                                   :resource-paths ["test-resources"]
                                   }
                        :uberjar  {:aot :all}
                        :provided {:dependencies [
                                                  [net.sf.kxml/kxml2 "2.3.0" :use-resources true]
-                                                 [reply/reply "0.1.0-beta9"]
-                                                 [org.clojure/tools.nrepl "0.2.6"]
-                                                 ]}}
+                                                 ;[reply/reply "0.1.0-beta9"]
+                                                 ;[org.clojure/tools.nrepl "0.2.6"]
+                                                 ]}
+                       :repl     {:dependencies   [[reply/reply "0.1.0-beta9"]
+                                                   [org.clojure/tools.nrepl "0.2.6"]
+                                                   [org.clojure/core.typed "0.3.0-alpha5"]
+                                                   ]
+                                  :resource-paths ["test-resources"]
+                                  }
+                       }
+
 
             :source-paths ["src/clojure" "src"]
             :main ^:skip-aot ru.vif.model.vif-xml-tools
--- vif-navigator2/project.clj
+++ vif-navigator2/project.clj
@@ -14,22 +14,26 @@
                            [neko/neko "3.2.0"]
                            [org.jsoup/jsoup "1.8.2" :use-resources true]
                            [vif-navigator-libs2 "0.1.0-SNAPSHOT" :use-resources true]
+                           [org.clojure/core.typed.rt "0.3.0-alpha5"]
                            ;[org.clojure/core.async "0.1.346.0-17112a-alpha"]
                            ]
-            :profiles {:default [:release]
+            :profiles {:default [:dev]
 
                        :dev
                                 [:android-common :android-user
                                  {:dependencies [[org.clojure-android/tools.nrepl "0.2.6"]]
                                   :target-path  "target/debug"
-                                  :android      {:aot                     :all-with-unused
-                                                 :manifest-options        {:app-name "VifNavigator - debug"}
-                                                 :target-version "15"
-                                                 :aot-exclude-ns [cljs.core.async.macros
-                                                                  cljs.core.impl-ioc-macros
-                                                                  cljs.core.async.impl.ioc-macros
-                                                                  clojure.core.async.lab
-                                                                  clojure.core.async]
+                                  :android      {
+                                                 :aot              :all-with-unused
+                                                 :manifest-options {:app-name "VifNavigator - debug"}
+                                                 :target-version   "15"
+                                                 :aot-exclude-ns   [cljs.core.async.macros
+                                                                    cljs.core.impl-ioc-macros
+
+                                                                    clojure.core.typed.all-envs
+                                                                    cljs.core.typed.async
+                                                                    clojure.core.typed.async
+                                                                    ]
                                                  }
                                   }]
                        :release
@@ -39,8 +43,8 @@
                                                {;; Specify the path to your private keystore
                                                 ;; and the the alias of the key you want to
                                                 ;; sign APKs with.
-                                                 :keystore-path "/home/mikl/.android/release-keysore"
-                                                 :key-alias "vif-alias"
+                                                :keystore-path       "/home/mikl/.android/release-keysore"
+                                                :key-alias           "vif-alias"
 
                                                 :ignore-log-priority [:debug :verbose]
                                                 :aot                 :all-with-unused
@@ -53,39 +57,38 @@
                                                            [vif-navigator-libs "0.1.0-SNAPSHOT" :use-resources true]
                                                            ]
 
-                                  :exclusions [[org.clojure/clojure]]
-                                  :jvm-opts ["-Dclojure.compile.ignore-lean-classes=true"]
-                                  :global-vars ^:replace {clojure.core/*warn-on-reflection* true}
-                                  :android {:lean-compile true
-                                            :skummet-skip-vars ["#'neko.init/init"
-                                                                "#'neko.context/context"
-                                                                "#'neko.resource/package-name"
-                                                                "#'neko.-utils/keyword->static-field"
-                                                                "#'neko.-utils/keyword->setter"
-                                                                "#'neko.ui.traits/get-display-metrics"
-                                                                "#'test.leindroid.sample.main/MainActivity-onCreate"
-                                                                "#'test.leindroid.sample.main/MainActivity-init"]}}]
+                                  :exclusions   [[org.clojure/clojure]]
+                                  :jvm-opts     ["-Dclojure.compile.ignore-lean-classes=true"]
+                                  :global-vars  ^:replace {clojure.core/*warn-on-reflection* true}
+                                  :android      {:lean-compile      true
+                                                 :skummet-skip-vars ["#'neko.init/init"
+                                                                     "#'neko.context/context"
+                                                                     "#'neko.resource/package-name"
+                                                                     "#'neko.-utils/keyword->static-field"
+                                                                     "#'neko.-utils/keyword->setter"
+                                                                     "#'neko.ui.traits/get-display-metrics"
+                                                                     "#'test.leindroid.sample.main/MainActivity-onCreate"
+                                                                     "#'test.leindroid.sample.main/MainActivity-init"]}}]
                        }
 
             :android {;; Specify the path to the Android SDK directory.
-                      ;:sdk-path       "/home/mzheludkov/work/clojure/android-sdk-linux"
-                      :sdk-path       "/home/mikl/distr/android-sdk-linux"
+                      :sdk-path          "/home/mzheludkov/work/clojure/android-sdk-linux"
+                      ;:sdk-path       "/home/mikl/distr/android-sdk-linux"
 
                       ;; Try increasing this value if dexer fails with
                       ;; OutOfMemoryException. Set the value according to your
                       ;; available RAM.
-                      :dex-opts       ["-JXmx4096M" "--num-threads=4" "--incremental"]
+                      :dex-opts          ["-JXmx4096M" "--num-threads=4" "--incremental"]
                       ;:dex-opts       ["-JXmx4096M" "--num-threads=4" ]
 
                       ;; If previous option didn't work, uncomment this as well.
                       ;; :force-dex-optimize true
                       :support-libraries ["v4"]
-                      :target-version "15"
-                      :aot-exclude-ns ["clojure.parallel" "clojure.core.reducers"
-                                       "cljs-tooling.complete" "cljs-tooling.info"
-                                       "cljs-tooling.util.analysis" "cljs-tooling.util.misc"
-                                       "cider.nrepl" "cider-nrepl.plugin" "ru.vif.http-client"]
-
+                      :target-version    "15"
+                      :aot-exclude-ns    ["clojure.parallel" "clojure.core.reducers"
+                                          "cljs-tooling.complete" "cljs-tooling.info"
+                                          "cljs-tooling.util.analysis" "cljs-tooling.util.misc"
+                                          "cider.nrepl" "cider-nrepl.plugin" "ru.vif.http-client"]
 
                       }
             :aliases {"build" [["droid" "doall" "-e"]]}
