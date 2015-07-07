2d6b41f7f07eaa9eba617ceaaed60cee324930ff
mzheludkov <mzheludkov@at-consulting.ru>
2015-07-07 17:13:52 +0300

    typed

--- vif-navigator-libs2/src/ru/vif/model/trim_tools.clj
+++ vif-navigator-libs2/src/ru/vif/model/trim_tools.clj
@@ -6,13 +6,15 @@
     [ru.vif.model.vif-xml-tools :refer :all]
     [ru.vif.model.model-tools :refer :all]
     [ru.vif.model.records :refer :all]
+    [clojure.core.typed :as t]
+    [ru.vif.model.typed-libs :refer :all]
     )
-  (:import (clojure.lang Keyword IPersistentMap)
+  (:import (clojure.lang Keyword IPersistentMap Counted)
            (ru.vif.model.records vif-xml-entry parse-data vif-tree vif-display-entry)
            ))
 
 
-
+(t/ann child-nodes-with-depth [LongLongMap Long -> (t/ASeq NoWithDepth)])
 (defn child-nodes-with-depth
   "Возвращает sequence дочерних элементов узла элементов с глубиной"
   ^{:private true}
@@ -21,25 +23,26 @@
   (->> (make-zipper parent-to-child-no-map no)
        child-nodes
        (map
-         (fn [loc]
-           [(zip/node loc) (count (zip/path loc))]
+         (t/fn [loc :- LongZipper] :- NoWithDepth
+           [(long-zipper-node loc) (long-count (zip/path loc))]
            )
          )
        )
   )
 
+(t/ann find-entry-in-tree [Long vif-tree -> (t/Option vif-xml-entry)])
 (defn find-entry-in-tree
   [^Long no, ^vif-tree current-vif-tree]
   (get (:all-entries-map current-vif-tree) no)
   )
 
-
+(t/ann init-display-entry [Long vif-tree Long -> (t/Option vif-display-entry)])
 (defn init-display-entry
   "создает record с данными одного изменения vif дерева"
   ^{:private true}
-  [^Long no, ^vif-tree current-vif-tree,  ^Long depth]
+  [^Long no, ^vif-tree current-vif-tree, ^Long depth]
 
-  (let [^vif-xml-entry xml-entry (find-entry-in-tree no current-vif-tree)]
+  (if-let [^vif-xml-entry xml-entry (find-entry-in-tree no current-vif-tree)]
     (vif-display-entry.
       no
       (:title xml-entry)
@@ -58,6 +61,9 @@
     )
   )
 
+(t/ann update-in-if (t/All [y]
+                           (t/IFn [y Boolean (t/Vec t/Any) [t/Any -> t/Any] -> y] )
+                           ))
 (defn update-in-if
   "update-in с условием"
   ^{:private true}
@@ -69,6 +75,7 @@
     )
   )
 
+(t/ann update-prev-entry [vif-display-entry Long vif-tree -> vif-display-entry])
 (defn update-prev-entry
   "Изменяет предыдущую запись в линейном списке записей, увеличивает количество дочерних записей, не посещенных, максимальный номер дочерней записи"
   ^{:private true}
@@ -76,10 +83,11 @@
    ^Long no
    ^vif-tree current-vif-tree]
 
-  (let [^vif-xml-entry xml-entry (find-entry-in-tree no current-vif-tree)
-        ^Boolean is-visited (:is_visited xml-entry)]
-    (-> update-entry
-        (update-in [:child-count] inc)
+  (t/let [^vif-xml-entry xml-entry :- (t/Option vif-xml-entry) (find-entry-in-tree no current-vif-tree)
+          ^Boolean is-visited :- (t/Option Boolean) (:is_visited xml-entry)
+          updated1 :- vif-display-entry (update-in update-entry [:child-count] inc)
+          ]
+    (-> updated1
         (update-in-if (not is-visited) [:non-visited-child-count] inc)
         (update-in [:max-child-no] max no)
         )
--- vif-navigator-libs2/src/ru/vif/model/typed_libs.clj
+++ vif-navigator-libs2/src/ru/vif/model/typed_libs.clj
@@ -1,7 +1,7 @@
 (ns ru.vif.model.typed-libs
   (:require [clojure.core.typed :as t]
             )
-  (:import (clojure.lang Keyword IPersistentMap IPersistentSet)))
+  (:import (clojure.lang Keyword IPersistentMap IPersistentSet Counted)))
 
 (t/defalias NillableString (t/U nil String))
 (t/defalias NillableLong (t/U nil Long))
@@ -16,6 +16,8 @@
 (t/defalias Zipper (t/All [x] (t/Seq x)))
 (t/defalias LongZipper (t/ASeq Long))
 (t/defalias LongLongMap (IPersistentMap Long NillableLongSet))
+(t/defalias LongInteger (t/U Integer Long))
+(t/defalias NoWithDepth (t/HVec [Long Long]))
 
 (t/ann clojure.core/not-empty (t/All [x]
                                      (t/IFn [t/Any -> (t/Option (t/Seq x))])
@@ -36,6 +38,8 @@
                                (t/IFn [(t/ASeq x) -> (t/ASeq x)])))
 (t/ann clojure.zip/node (t/All [x]
                                (t/IFn [(t/ASeq x) -> x])))
+(t/ann clojure.zip/path (t/All [x]
+                               (t/IFn [x -> (t/ASeq x)])))
 
 
 
@@ -45,11 +49,14 @@
 (t/non-nil-return org.xmlpull.v1.XmlPullParserFactory/newPullParser :all)
 (t/non-nil-return org.xmlpull.v1.XmlPullParser/getAttributeName :all)
 
-(t/ann clojure.core/update-in (t/All [x y]
-                                     (t/IFn [(t/ASeq x) (t/Vec t/Any) [x -> x] -> (t/ASeq x)]
-                                            [(t/Map y x) (t/Vec t/Any) [x -> x] -> (t/Map y x)]
-                                            [y (t/Vec t/Any) [x -> x] -> y]
-                                            )))
+(t/ann clojure.core/update-in
+         (t/All [x y]
+                (t/IFn [(t/ASeq x) (t/Vec t/Any) [x -> x] -> (t/ASeq x)]
+                       [(t/Map y x) (t/Vec t/Any) [x -> x] -> (t/Map y x)]
+                       [y (t/Vec t/Any) [t/Any * -> t/Any] -> y]
+                       [y (t/Vec t/Any) [t/Any -> t/Any] -> y]
+                       ))
+       )
 
 (t/ann clojure.core/assoc-in (t/All [x y]
                                     (t/IFn [(t/Map y x) (t/Vec t/Any) t/Any -> (t/Map y x)]
@@ -60,3 +67,6 @@
                                         [(t/Vec d) t/AnyInteger d -> (t/Vec d)]
                                         [d t/Any t/Any -> d]
                                         )))
+
+(t/ann ^:no-check long-count [(t/U (t/Seqable t/Any) Counted nil) -> Long :object {:path [Count], :id 0}])
+(def long-count  count)
